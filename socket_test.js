bb73f6eb26cc1d84f7e827843d7c66d26af43c24
손준혁 <thsty91@gmail.com>
2016-10-28 11:41:54 +0900

    made index file and update socket.test

--- socket_test.js
+++ socket_test.js
@@ -1,5 +1,24 @@
-var http = require('http');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var fs = require('fs');
-var socketio = require('socket.io');
+var path = require('path');
 
+app.get('/chat', (req,res) => {
+  res.sendFile(path.join(__dirname,'index.html'));
+});
 
+app.get('/src', (req, res) => {
+  res.seanFire('node_modules/socket.io/lib/socket.js');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('sMsg', (data) => {
+    io.emit('rMsg',data);
+  });
+});
+
+app.listen(3300, () => {
+  console.log("3300 connected!")
+});
